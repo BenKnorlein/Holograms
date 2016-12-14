995dd6f9a89c674bf9db79b6dc49f51dd1a2e658
Benjamin_Knorlein@brown.edu <Benjamin_Knorlein@brown.edu>
2016-12-14 11:32:42 -0500

    Added program to generate background images and changed writeImages to support background images

---
+++ src/generateBackground.cpp
@@ -0,0 +1,80 @@
+///\file main.cpp
+///\author Benjamin Knorlein
+///\date 08/10/2016
+
+#include <iostream>
+
+#ifdef _MSC_VER
+	#define _CRT_SECURE_NO_WARNINGS
+
+	#ifndef WITH_CONSOLE
+		#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+	#endif
+
+	#include <windows.h>
+#else
+	#include <sys/stat.h>
+	#include <sys/types.h>
+#endif
+
+#include <fstream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace cv;
+
+std::string slash = "/"; 
+
+template <typename It>
+typename std::iterator_traits<It>::value_type Median(It begin, It end)
+{
+	auto size = std::distance(begin, end);
+		std::nth_element(begin, begin + size / 2, end);
+	return *std::next(begin, size / 2);
+}
+
+int main(int argc, char** argv)
+{
+
+	if (argc < 3)
+	{
+		std::cerr << "You need to provide an output filename and a list of images in a txt file" << std::endl;
+		return 0;
+	}
+
+	std::string filename_out = std::string(argv[1]);
+	std::string filename = std::string(argv[2]);
+
+	std::vector<cv::Mat> images;
+
+	std::ifstream infile(filename);
+	std::string line;
+	while (std::getline(infile, line))
+	{
+		std::istringstream iss(line);
+		cv::Mat im = cv::imread(line);
+		images.push_back(im);
+	}
+
+	cv::Mat im_out;
+	images[0].copyTo(im_out);
+
+	for (int y = 0; y < im_out.rows; y++){
+		for (int x = 0; x < im_out.cols; x++){
+			//std::cerr << (int)im_out.at<cv::Vec3b>(y, x)[0] << " " << (int)im_out.at<cv::Vec3b>(y, x)[1] << " " << (int)im_out.at<cv::Vec3b>(y, x)[2] << std::endl;
+
+			std::vector<unsigned char> values;
+			for (int i = 0; i < images.size(); i++){
+				values.push_back(images[i].at<cv::Vec3b>(y, x)[0]);
+			}
+			unsigned char out_value = Median(values.begin(), values.end());
+			im_out.at<cv::Vec3b>(y, x)[0] = out_value;
+			im_out.at<cv::Vec3b>(y, x)[1] = out_value;
+			im_out.at<cv::Vec3b>(y, x)[2] = out_value;
+		}
+	}
+
+	cv::imwrite(filename_out, im_out);
+	return 1;
+}
+
--- src/writeImages.cpp
+++ src/writeImages.cpp
@@ -26,7 +26,7 @@ using namespace cv;
 
 std::string slash = "/"; 
 
-void writeImages(std::string filename, Settings * settings)
+void writeImages(std::string filename, Settings * settings, std::string background = "")
 {
 	int width = settings->getWidth();
 	int height = settings->getHeight();
@@ -45,16 +45,20 @@ void writeImages(std::string filename, Settings * settings)
 
 	OctopusClient * client = new OctopusClient(settings->getIp(), settings->getPort());
 
-	std::cout << "Loading " << datafolder + "/" + filename  << " with Background" << std::endl;
-	if (!client->setSourceHologram(datafolder + "/", filename , "background.bmp"))exit;
+	std::cout << "Loading " << datafolder + "/" + filename << std::endl;
+	if (background != ""){
+		std::cout << "Use Background " << datafolder + "/" + background << std::endl;
+	}
+
+	if (!client->setSourceHologram(datafolder + "/", filename, background))exit;
 
 	std::string name;
 	for (int d = start; d <= stop; d += step_width){
-		std::cerr << "Save amplitude " << d << " with Background" << std::endl;
+		std::cerr << "Save amplitude " << d << std::endl;
 		float* data = new float[width * height];
 		client->getAmplitudeImage(d, data);
 
-		name = outFile + slash + "AmplitudeB_" + std::to_string(d) + ".ext";
+		name = outFile + slash + "Amplitude_" + std::to_string(d) + ".ext";
 		FILE* file = fopen(name.c_str(), "wb");
 		fwrite(data, sizeof(float), width * height, file);
 		fclose(file);
@@ -66,18 +70,18 @@ void writeImages(std::string filename, Settings * settings)
 		normalize(image, image_disp, 0, 255, CV_MINMAX);
 		image_disp.convertTo(B, CV_8U);
 
-		imwrite(outFile + slash + "AmplitudeB_" + std::to_string(d) + ".png", B);
+		imwrite(outFile + slash + "Amplitude_" + std::to_string(d) + ".png", B);
 
 		image.release();
 		delete[] data;
 	}
 
 	for (int d = start; d <= stop; d += step_width){
-		std::cerr << "Save amplitude " << d << " with Background" << std::endl;
+		std::cerr << "Save Intensity " << d << std::endl;
 		float* data = new float[width * height];
 		client->getIntensityImage(d, data);
 
-		name = outFile + slash + "IntensityB_" + std::to_string(d) + ".ext";
+		name = outFile + slash + "Intensity_" + std::to_string(d) + ".ext";
 		FILE* file = fopen(name.c_str(), "wb");
 		fwrite(data, sizeof(float), width * height, file);
 		fclose(file);
@@ -89,63 +93,12 @@ void writeImages(std::string filename, Settings * settings)
 		normalize(image, image_disp, 0, 255, CV_MINMAX);
 		image_disp.convertTo(B, CV_8U);
 
-		imwrite(outFile + slash + "IntensityB_" + std::to_string(d) + ".png", B);
+		imwrite(outFile + slash + "Intensity_" + std::to_string(d) + ".png", B);
 
 		image.release();
 		delete[] data;
 	}
 
-	for (int d = start; d <= stop; d += step_width){	
-		std::cerr << "Save Phase " << d << " with Background" << std::endl;
-		if (!client->setSourceHologram(datafolder + "/", filename))exit;
-
-		float* data1 = new float[width * height];
-		client->getPhaseImage(d, data1);
-
-		if (!client->setSourceHologram(datafolder + "/", "background.bmp"))exit;
-		float* data2 = new float[width * height];
-		client->getPhaseImage(d, data2);
-
-		float* data3 = new float[width * height];
-
-		float* dt1_ptr = data1;
-		float* dt2_ptr = data2;
-		float* dt3_ptr = data3;
-
-		for (int i = 0; i < width*height; i++, ++dt1_ptr, ++dt2_ptr, ++dt3_ptr)
-		{
-			*dt3_ptr = *dt1_ptr - *dt2_ptr;
-		}
-
-		name = outFile + slash + "PhaseB_" + std::to_string(d) + ".ext";
-		FILE* file = fopen(name.c_str(), "wb");
-		fwrite(data3, sizeof(float), width * height, file);
-		fclose(file);
-
-		//cv::Mat image1(cv::Size(width, height), CV_32FC1, data1);
-		//cv::Mat image2(cv::Size(width, height), CV_32FC1, data2);
-		cv::Mat image3(cv::Size(width, height), CV_32FC1, data3);
-		cv::Mat image_disp;
-		cv::Mat B;
-		//normalize(image1, image_disp, 0, 255, CV_MINMAX);
-		//image_disp.convertTo(B, CV_8U);
-		//imwrite(outputFolder + slash + "Phase1B_" + std::to_string(d) + ".png", B);
-		//normalize(image2, image_disp, 0, 255, CV_MINMAX);
-		//image_disp.convertTo(B, CV_8U);
-		//imwrite(outputFolder + slash + "Phase2B_" + std::to_string(d) + ".png", B);
-		normalize(image3, image_disp, 0, 255, CV_MINMAX);
-		image_disp.convertTo(B, CV_8U);
-		imwrite(outFile + slash + "PhaseB_" + std::to_string(d) + ".png", B);
-
-		//image1.release();
-		//image2.release();
-		image3.release();
-		delete[] data1;
-		delete[] data2;
-		delete[] data3;
-	}
-
-	//set Image
 	std::cout << "Loading " << datafolder + "/" + filename << std::endl;
 	if (!client->setSourceHologram(datafolder + "/", filename))exit;
 
@@ -172,53 +125,6 @@ void writeImages(std::string filename, Settings * settings)
 		image.release();
 		delete[] data;
 	}
-
-	for (int d = start; d <= stop; d += step_width){
-		std::cerr << "Save amplitude " << d << std::endl;
-		float* data = new float[width * height];
-		client->getAmplitudeImage(d, data);
-
-		name = outFile + slash + "Amplitude_" + std::to_string(d) + ".ext";
-		FILE* file = fopen(name.c_str(), "wb");
-		fwrite(data, sizeof(float), width * height, file);
-		fclose(file);
-
-		cv::Mat image(cv::Size(width, height), CV_32FC1, data);
-
-		cv::Mat image_disp;
-		cv::Mat B;
-		normalize(image, image_disp, 0, 255, CV_MINMAX);
-		image_disp.convertTo(B, CV_8U);
-
-		imwrite(outFile + slash + "Amplitude_" + std::to_string(d) + ".png", B);
-
-		image.release();
-		delete[] data;
-	}
-
-
-	for (int d = start; d <= stop; d += step_width){
-		std::cerr << "Save Intensity " << d << std::endl;
-		float* data = new float[width * height];
-		client->getIntensityImage(d, data);
-
-		name = outFile + slash + "Intensity_" + std::to_string(d) + ".ext";
-		FILE* file = fopen(name.c_str(), "wb");
-		fwrite(data, sizeof(float), width * height, file);
-		fclose(file);
-
-		cv::Mat image(cv::Size(width, height), CV_32FC1, data);
-
-		cv::Mat image_disp;
-		cv::Mat B;
-		normalize(image, image_disp, 0, 255, CV_MINMAX);
-		image_disp.convertTo(B, CV_8U);
-
-		imwrite(outFile + slash + "Intensity_" + std::to_string(d) + ".png", B);
-
-		image.release();
-		delete[] data;
-	}
 }
 
 int main(int argc, char** argv)
@@ -233,7 +139,11 @@ int main(int argc, char** argv)
 	std::string filename = std::string(argv[1]);
 	Settings * settings = new Settings(argv[2]);
 
-	writeImages(filename, settings);
+	std::string background = "";
+
+	if (argc >= 3) background = std::string(argv[3]);
+
+	writeImages(filename, settings, background);
 
 	delete settings;
 	return 1;
